d98e5bc7974da856614abbf527e1c5ffb79e75ab
ls <qqlishuang@gmail.com>
2021-10-11 11:21:33 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-python train.py --img 640 --batch 4 --epochs 50 --weights weights/yolov5s_v4.pt --data data/coco_hand.yaml --cfg models/yolov5s.yaml --name s_hand
+python train.py --img 416 --batch 16 --epochs 50 --weights weights/yolov5s_v4.pt --data data/coco_hand.yaml --cfg models/yolov5s.yaml --name s_hand --cache
 
