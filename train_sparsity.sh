0a29c3cbeb8b59e64c36fbda058d9deb7d6f00b1
ls <qqlishuang@gmail.com>
2021-10-13 16:48:50 +0800

    remove blank line

--- train_sparsity.sh
+++ train_sparsity.sh
@@ -1,2 +1,1 @@
 python train_sparsity.py --img 416 --batch 8 --epochs 50 --data data/coco_hand.yaml --cfg models/yolov5s.yaml --weights runs/train/s_hand/weights/last.pt --name s_hand_sparsity -sr --s 0.001 --prune 1
-
